5c57a8b9460167947e79f9afc85fe764692c96e3
CavillotGregory <gregorycavillot0@gmail.com>
2025-06-16 10:11:10 +0200

    changement

--- accueil.php
+++ accueil.php
@@ -3,7 +3,13 @@
     require ('header.php');
 ?>
 
-
+<main class="container">
+  <div class="bg-body-tertiary p-5 rounded">
+    <h1>Test</h1>
+    <p class="lead">Page Accueil</p>
+    <a class="btn btn-lg btn-primary" href="/docs/5.3/components/navbar/" role="button">View navbar docs &raquo;</a>
+  </div>
+</main>
 
 
 
--- note.php
+++ note.php
@@ -4,7 +4,13 @@ require('header.php');
 ?>
 
 
-
+<main class="container">
+  <div class="bg-body-tertiary p-5 rounded">
+    <h1>Test</h1>
+    <p class="lead">Page note</p>
+    <a class="btn btn-lg btn-primary" href="/docs/5.3/components/navbar/" role="button">View navbar docs &raquo;</a>
+  </div>
+</main>
 
 
 <?php
