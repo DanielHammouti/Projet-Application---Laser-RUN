4968e4a3ece49c9c422a2f2721c59f7cb5340740
vstadle <valentin.stadlerpro@icloud.com>
2025-06-17 13:45:03 +0200

    Fix timer

--- src/js/shooting_timer.js
+++ src/js/shooting_timer.js
@@ -48,13 +48,6 @@ const ShootingTimer = {
 
     finishButton.onclick = () => {
       sendSessionDataToAPI();
-
-      localStorage.setItem('sixTime', '0');
-      localStorage.setItem('fourTime', '0');
-      localStorage.setItem('twoTime', '0');
-      localStorage.setItem('sixmeter', '0');
-      localStorage.setItem('nbTirs', '0');
-
       //window.location.href = '../html/mark_page.html';
     };
 
