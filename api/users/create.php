621fd6b422c38bba141f46d1943faeffe32a3c63
vstadle <valentin.stadlerpro@icloud.com>
2025-06-17 10:01:15 +0200

    Tentative fix api create

--- api/users/create.php
+++ api/users/create.php
@@ -23,7 +23,7 @@ try{
     $user = new User($database->conn);
 
     // Récupération des données POST
-    $data = json_decode(file_get_contents("php://input"), true);
+    $data = $_POST;
     
     if(!isset($data['id']) || !isset($data['nom']) || !isset($data['prenom']) || !isset($data['groupe']) || !isset($data['classe']) || !isset($data['sexe'])){
         throw new Exception("Les données sont invalides");
