0c3b9bcdcd10d65679112e6c70f5d1b6c45fda3e
Carla <carla.le-sommier@etu.univ-nantes.fr>
2025-06-17 14:33:05 +0200

    date

--- src/php/note.php
+++ src/php/note.php
@@ -8,7 +8,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Liste des utilisateurs</title>
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/style_admin.css">
 </head>
 <body>
     <h1>Liste des utilisateurs</h1>
