9267dc7c0e2e2d2e0a4e3a96d01f90e64a3af2fd
Valentin STADLER <valentin.stadler@universite-paris-saclay.fr>
2025-06-24 19:26:23 +0200

    Ajout vérification accès à la page note

--- www/src/js/verify_user.js
+++ www/src/js/verify_user.js
@@ -1,5 +1,6 @@
 let verifauthuser = localStorage.getItem("userId");
 
 if (verifauthuser !== "L4iXbUG6eKPdn14mO8ObMqKM8jp1") {
+    console.log("Vous n'avez pas les droits d'accès à cette page");
     window.location.href = "https://172.16.100.3/src/html/index.html";
 }
