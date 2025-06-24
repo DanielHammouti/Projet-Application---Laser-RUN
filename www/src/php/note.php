d880aaebc214d2989902db5f69e1f989008dce8d
Valentin STADLER <valentin.stadler@universite-paris-saclay.fr>
2025-06-24 19:23:32 +0200

    Ajout vérification accès à la page note

--- www/src/php/note.php
+++ www/src/php/note.php
@@ -38,6 +38,13 @@
 
     <script >
             document.addEventListener("DOMContentLoaded", function () {
+
+                let verifauthuser = localStorage.getItem("userId");
+
+                if (verifauthuser !== "L4iXbUG6eKPdn14mO8ObMqKM8jp1") {
+                    window.location.href = "https://172.16.100.3/src/html/index.html";
+                }
+
                 fetch("https://172.16.100.3/api/users/read.php", {
                     credentials: "include"
                 })
